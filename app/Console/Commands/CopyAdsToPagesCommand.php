b875dc0ebf3b98cacee4428203709bdf0f632818
Oleg Shabashov <oleg.shabashov@searchanise.io>
2024-03-16 11:48:28 +0300

    Add typesense and meilisearch to copy-ads-to-pages

--- app/Console/Commands/CopyAdsToPagesCommand.php
+++ app/Console/Commands/CopyAdsToPagesCommand.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Meilisearch;
+use App\Models\Typesense;
+use Artisan;
 use Illuminate\Console\Command;
 use Illuminate\Support\Benchmark;
 use Illuminate\Support\Collection;
@@ -9,34 +12,68 @@ use Illuminate\Support\Facades\DB;
 
 class CopyAdsToPagesCommand extends Command
 {
-    protected $signature = 'copy:ads-to-pages';
+    protected $signature = 'copy:ads-to-pages
+    {--driver= : The driver to be used: pgsql, meilisearch or typesense}';
 
     protected $description = 'Command description';
 
     public function handle(): void
     {
         $faker = fake();
-        $chunk = 10_000;
-        $bar = $this->output->createProgressBar(500_000);
+        $chunk = 1000;
+        $bar   = $this->output->createProgressBar(500_000);
         $bar->start();
 
+        match ($this->option('driver')) {
+            'meilisearch' => $this->meilisearchInsert($bar, $chunk),
+            'typesense'   => $this->typesenseInsert($bar, $chunk),
+            'pgsql'       => $this->pgsqlInsert($faker, $bar, $chunk),
+        };
+
+        $bar->finish();
+    }
+
+    private function meilisearchInsert($bar, $chunk): void
+    {
+        Artisan::call('scout:flush', ["model" => Meilisearch\Page::class]);
+
+        Meilisearch\Page::chunk($chunk, function (Collection $pages) use ($chunk, $bar) {
+            [, $duration] = Benchmark::value(fn() => $pages->searchable());
+            $bar->advance($chunk);
+
+            $this->info("Searchable {$bar->getProgress()} pages in $duration seconds");
+        });
+    }
+
+    private function typesenseInsert($bar, $chunk): void
+    {
+        Artisan::call('scout:flush', ["model" => Typesense\Page::class]);
+
+        Typesense\Page::chunk($chunk, function (Collection $pages) use ($chunk, $bar) {
+            [, $duration] = Benchmark::value(fn() => $pages->searchable());
+            $bar->advance($chunk);
+
+            $this->info("Searchable {$bar->getProgress()} pages in $duration seconds");
+        });
+    }
+
+    private function pgsqlInsert($faker, $bar, $chunk): void
+    {
         DB::table('ads')->orderBy('id')->chunk($chunk, function (Collection $ads) use ($faker, $bar, $chunk) {
-//            DB::table('pages')->truncate();
             $durations = collect();
             foreach ($ads as $ad) {
-                [$count, $duration] = Benchmark::value(fn () => DB::table('pages')->insert([
-                    'title'      => $faker->title,
-                    'url'        => $faker->url,
-                    'sections'   => $ad->json,
+                [, $duration] = Benchmark::value(fn() => DB::table('pages')->insert([
+                    'title'    => $faker->title,
+                    'url'      => $faker->url,
+                    'sections' => $ad->json,
                 ]));
 
                 $durations->push($duration);
             }
 
             $bar->advance($chunk);
+
             $this->info("Inserted {$bar->getProgress()} pages in {$durations->avg()} seconds");
         });
-
-        $bar->finish();
     }
 }
