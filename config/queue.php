8dfe04dd8c6dca1503138ffcc73c6a7d05e80310
Oleg Shabashov <oleg.shabashov@gmail.com>
2024-09-05 10:51:21 +0700

    Listen cassie_high queue by default

--- config/queue.php
+++ config/queue.php
@@ -65,7 +65,7 @@ return [
         'redis' => [
             'driver'       => 'redis',
             'connection'   => 'default',
-            'queue'        => env('REDIS_QUEUE', 'default'),
+            'queue'        => env('REDIS_QUEUE', 'cassie_high'),
             'retry_after'  => 90,
             'block_for'    => null,
             'after_commit' => false,
