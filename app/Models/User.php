fdfe68e0fc469a2f502b9e3e9008dcd6c2bc87f4
Oleg Shabashov <oleg.shabashov@gmail.com>
2024-09-16 17:02:20 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,19 +12,17 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
      * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
+        'email_verified_at',
+        'shopify_access_token',
         'password',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
      * @var array<int, string>
      */
     protected $hidden = [
@@ -33,12 +31,13 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
+     * @return array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password'          => 'hashed',
-    ];
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password'          => 'hashed',
+        ];
+    }
 }
