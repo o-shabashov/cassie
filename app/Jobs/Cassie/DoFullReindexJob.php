7c78402d6e04ef9594dd5b39c1533bb079abf41c
Oleg Shabashov <oleg.shabashov@gmail.com>
2024-10-16 11:35:19 +0700

    Rename namespace

--- app/Jobs/Cassie/DoFullReindexJob.php
+++ app/Jobs/Cassie/DoFullReindexJob.php
@@ -4,9 +4,9 @@ namespace App\Jobs\Cassie;
 
 use App\Exceptions\ShopifyProductException;
 use App\GraphQL\ShopifyProduct;
-use App\Models\Meilisearch;
+use App\Models\Meilisearch\MeilisearchProduct;
 use App\Models\Product;
-use App\Models\Typesense;
+use App\Models\Typesense\TypesenseProduct;
 use Exception;
 use Gnikyt\BasicShopifyAPI\BasicShopifyAPI;
 use Illuminate\Support\Facades\Artisan;
@@ -20,6 +20,7 @@ class DoFullReindexJob extends BaseCassieHighQueueJob
         try {
             $api = app(BasicShopifyAPI::class, ['shop' => $this->shopifyUserDto->name, 'accessToken' => $this->shopifyUserDto->password]);
 
+            // TODO iterate over all products
             // 1. Insert all products to the database
             collect(data_get(ShopifyProduct::get($api), 'body.container.data.products.nodes'))
                 ->each(function ($product) {
@@ -41,12 +42,12 @@ class DoFullReindexJob extends BaseCassieHighQueueJob
             // TODO flush and reindex in different queues - high for main and low for the backup
 
             // 2. Send all products to the main search engine
-            Artisan::call('scout:flush', ['model' => Meilisearch\MeilisearchProduct::class]);
-            Artisan::call('scout:flush', ['model' => Typesense\TypesenseProduct::class]);
+            Artisan::call('scout:flush', ['model' => MeilisearchProduct::class]);
+            Artisan::call('scout:flush', ['model' => TypesenseProduct::class]);
 
             // 3. Send all products to the backup search engine
-            Meilisearch\MeilisearchProduct::all()->searchable();
-            Typesense\TypesenseProduct::all()->searchable();
+            MeilisearchProduct::all()->searchable();
+            TypesenseProduct::all()->searchable();
         } catch (Exception $e) {
             if ($e instanceof ShopifyProductException) {
                 $error = data_get($$e->response->getDecodedBody(), 'errors');
